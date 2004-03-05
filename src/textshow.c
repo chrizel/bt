b6f257f350ca3f7b8fab32d3ffc8c11f91bc9ab1
Simon Goller <neosam@gmail.com>
2004-03-05 17:00:07 +0000

    Now I fixed the bug ;)

--- src/textshow.c
+++ src/textshow.c
@@ -1,7 +1,20 @@
-Repository : ladldadl@cvs.btgame.berlios.de:/cvsroot/btgame
-Module     : bt/src
-Working dir: ~/Dokumente/Programmierung/bt/src/
+#include "rotozoom/SDL_rotozoom.h"
+#include "event.h"
+#include "bt.h"
 
+SDL_Surface *background;
 
+static void textshow_paint()
+{
+    SDL_BlitSurface(background, NULL, screen, NULL);
+}
 
-
+int init_textshow()
+{
+    reg_func(textshow_paint, EH_PAINT);
+    background = (SDL_Surface *) bmpl_get("font.bg.test");
+    if(background == NULL)
+        printf("Warning: Failed to load image\n");
+    //background = zoomSurface(background, 
+    //        (double)screen->w/640.0, (double)screen->w/640.0, 0);
+}
