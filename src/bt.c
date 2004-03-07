d1454359db3e3ce87a46dd7453d0ebfa95c6570e
Christian Zeller <chrizel@gmail.com>
2004-03-07 21:35:01 +0000

    changed licence info

--- src/bt.c
+++ src/bt.c
@@ -37,32 +37,11 @@ static void init_sdl(void);
 static void keydown();
 static void quit();
 
-const char *GNU_TEXT =  
-"   This program is free software; you can redistribute it and/or modify\n"
-"   it under the terms of the GNU General Public License as published by\n"
-"   the Free Software Foundation; either version 2 of the License, or\n"
-"   (at your option) any later version.\n"
-"\n"
-"   This program is distributed in the hope that it will be useful,\n"
-"   but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-"   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-"   GNU General Public License for more details.\n"
-"\n"
-"   You should have received a copy of the GNU General Public License\n"
-"   along with this program; if not, write to the Free Software\n"
-"   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n";
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
-#include <SDL.h>
-
-#include "bt.h"
-#include "error.h"
-#include "event.h"
-#include "rotozoom/SDL_rotozoom.h"
-#include "m_math.h"
+const char GPL_TEXT[] =  
+"Bermuda Triangle, Copyright (C) 2004 Christian Zeller and Simon Goller\n"
+"Bermuda Triangle comes with ABSOLUTELY NO WARRANTY. This is free software,\n" 
+"and you are welcome to redistribute it under certain conditions; see\n"
+"LICENSE file for details.\n\n";
 
 int main(int argc, char *argv[])
 {
@@ -72,7 +51,7 @@ int main(int argc, char *argv[])
     cur_filter = NULL;
     con_last_param = NULL;
 
-    printf("%s", GNU_TEXT);
+    puts(GPL_TEXT);
 
     init_sdl();
     init_colors();
