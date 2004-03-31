e45e2315c806103eaaa8934e75a77666209ae135
Simon Goller <neosam@gmail.com>
2004-03-31 21:16:20 +0000

    Added a audio class

---
+++ src/audio.cpp
@@ -0,0 +1,21 @@
+#include "audio.h"
+
+Audio::Audio()
+{
+    // TODO
+}
+
+Audio::~Audio()
+{
+    // TODO
+}
+
+int Audio::AddMusic(char *filename)
+{
+    // TODO
+}
+
+int Audio::AddChunk(char *filename)
+{
+    // TODO
+}
